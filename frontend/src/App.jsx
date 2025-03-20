70589a83fce35ab6029739f18685ad2fb245efdb
vishal pathak <vishalpathak842@gmail.com>
2025-03-20 14:58:31 +0530

    Add routing for Appointments page with dynamic doctor ID

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import About from './pages/About'
 import Contact from './pages/Contact'
 import MyProfile from './pages/MyProfile'
 import MyAppointments from './pages/MyAppointments'
+import Appointments from './pages/Appointments'
 
 const App = () => {
   return (
@@ -21,6 +22,7 @@ const App = () => {
           <Route path='/contact' element={<Contact/>}/>
           <Route path='/my-profile' element={ <MyProfile/>}/>
           <Route path='/my-appointments' element={ <MyAppointments/>}/>
+          <Route path='/appointments/:docId' element={<Appointments/>}/>
           
         </Routes>
     </div>
