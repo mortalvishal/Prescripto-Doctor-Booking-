ec21a5c7ab3c1d5bd4fb62ad5f8e847d5868796e
vishal pathak <vishalpathak842@gmail.com>
2025-03-21 14:09:19 +0530

    Refactor Header component for improved readability and formatting

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -12,16 +12,26 @@ const Header = () => {
         <div className="flex flex-col md:flex-row items-center gap-3 text-white text-sm font-light">
           <img className="w-28" src={assets.group_profiles} alt="" />
           <p>
-            Simply browse through our extensive list of trusted doctors, <br className="hidden sm:block" /> schedule your appointment hassle-free.
+            Simply browse through our extensive list of trusted doctors,{" "}
+            <br className="hidden sm:block" /> schedule your appointment
+            hassle-free.
           </p>
         </div>
-        <a className="flex items-center bg-white gap-2 px-8 py-3 rounded-full text-gray-600 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300" href="#speciality">
-          Book Appointments <img className="w-3 " src={assets.arrow_icon} alt="" />
+        <a
+          className="flex items-center bg-white gap-2 px-8 py-3 rounded-full text-gray-600 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300"
+          href="#speciality"
+        >
+          Book Appointments{" "}
+          <img className="w-3 " src={assets.arrow_icon} alt="" />
         </a>
       </div>
       {/* -----------Right Side----------- */}
       <div className="md:w-1/2 relative">
-        <img className="w-full md:absolute bottom-0 h-auto rounded-lg" src={assets.header_img} alt="" />
+        <img
+          className="w-full md:absolute bottom-0 h-auto rounded-lg"
+          src={assets.header_img}
+          alt=""
+        />
       </div>
     </div>
   );
