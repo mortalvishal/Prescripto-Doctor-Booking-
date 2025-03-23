6ed8b67d20855c2b8d386f49aef08ff6d6a1eb9a
vishal pathak <vishalpathak842@gmail.com>
2025-03-23 13:35:14 +0530

    Add RelatedDoctors component to Appointments page

--- frontend/src/pages/Appointments.jsx
+++ frontend/src/pages/Appointments.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { AppContext } from '../context/AppContext'
 import { assets } from '../assets/assets'
+import RelatedDoctors from '../components/RelatedDoctors'
 
 const Appointments = () => {
 
@@ -124,7 +125,8 @@ const Appointments = () => {
         </div>
         <button className='bg-primary text-white text-sm font-light px-14 py-3 rounded-full my-6'>Book an appointment</button>
       </div>
-
+          {/* Listing Related doctors */}
+          <RelatedDoctors/>
     </div>
   )
 }
