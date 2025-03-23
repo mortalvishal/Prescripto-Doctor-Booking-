817c2850f96981166a3dae907b884083229b5cc5
vishal pathak <vishalpathak842@gmail.com>
2025-03-23 13:06:13 +0530

    Add availability slots fetching and display in Appointments component

--- frontend/src/pages/Appointments.jsx
+++ frontend/src/pages/Appointments.jsx
@@ -8,17 +8,69 @@ const Appointments = () => {
   const { docId } = useParams()
   const {doctors,currencySymbol} = useContext(AppContext)
   const [docInfo, setDocInfo] = useState(null)
+  const [docSlots,setDocSlots] = useState([])
+  const [slotindex,setSlotIndex] = useState(0)
+  const [slotTime,setSlotTime] = useState('')
 
   const fetchDocInfo = async () =>{
     const docInfo = doctors.find(doc => doc._id === docId)
     setDocInfo(docInfo)
-    console.log(docInfo);
-    
+  }
+
+  const getAvailableSlots = async () =>{
+    setDocSlots([])
+
+    //getting current date
+    let today = new Date()
+    for(let i=0;i<7;i++){
+      // getting date with index
+      let currentDate = new Date(today)
+      currentDate.setDate(today.getDate()+i)
+
+      // setting end time of the date with index
+      let endTime = new Date()
+      endTime.setDate(today.getDate()+1)
+      endTime.setHours(21,0,0,0)
+
+      // setting hours
+      if (today.getDate() === currentDate.getDate()) {
+        currentDate.setHours(currentDate.getHours() >10 ? currentDate.getHours()+1 :10)
+        currentDate.setMinutes(currentDate.getMinutes() > 30 ? 30:0)
+      }
+      else{
+        currentDate.setHours(10)
+        currentDate.setMinutes(0)
+      }
+
+      let timeSlots =[]
+
+      while(currentDate < endTime ){
+        let formattedTime = currentDate.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})
+
+        // add slots to array
+        timeSlots.push({
+          datetime: new Date(currentDate),
+          time: formattedTime,
+        })
+        // increment current time by 30 minute
+        currentDate.setMinutes(currentDate.getMinutes()+30)
+      }
+      setDocSlots(prev => ([...prev,timeSlots]))
+    }
   }
 
   useEffect(()=>{
     fetchDocInfo()
   },[doctors, docId])
+
+  useEffect(()=>{
+    getAvailableSlots()
+  },[docInfo])
+
+  useEffect(()=>{
+    console.log(docSlots);
+    
+  },[])
 
   return docInfo &&  (
     <div>
@@ -39,7 +91,8 @@ const Appointments = () => {
             <p className='flex items-center gap-1 text-sm font-medium text-gray-900 mt-3'>About <img src={assets.info_icon} alt=''/></p>
             <p className='text-sm text-gray-500 max-w-[700px] mt-1'>{docInfo.about}</p>
           </div>
-          <p className='text-gray-500 font-medium mt-4'>Appointment fee:<spna className='text-gray-600'> {currencySymbol}{docInfo.fees}</spna></p>
+          <p className='text-gray-500 font-medium mt-4'>Appointment fee:
+          <span className='text-gray-600'>{currencySymbol}{docInfo.fees}</span></p>
         </div>
        </div>
     </div>
