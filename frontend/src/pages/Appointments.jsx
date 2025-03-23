48e9ab7555936dd8f09a06e87f689725780c4a31
vishal pathak <vishalpathak842@gmail.com>
2025-03-23 13:31:32 +0530

    Add booking slots and time slots display in Appointments component

--- frontend/src/pages/Appointments.jsx
+++ frontend/src/pages/Appointments.jsx
@@ -7,6 +7,8 @@ const Appointments = () => {
 
   const { docId } = useParams()
   const {doctors,currencySymbol} = useContext(AppContext)
+  const daysOfWeek = ['SUN','MON','TUE','WED','THU','FRI','SAT']
+
   const [docInfo, setDocInfo] = useState(null)
   const [docSlots,setDocSlots] = useState([])
   const [slotIndex,setSlotIndex] = useState(0)
@@ -68,8 +70,7 @@ const Appointments = () => {
   },[docInfo])
 
   useEffect(()=>{
-    console.log(docSlots);
-    
+    console.log(docSlots); 
   },[docSlots])
 
   return docInfo &&  (
@@ -95,6 +96,34 @@ const Appointments = () => {
           <span className='text-gray-600'>{currencySymbol}{docInfo.fees}</span></p>
         </div>
        </div>
+
+
+      {/* --------------Booking Slots---------------- */}
+      <div className='sm:ml-72 sm:pl-4 mt-4 font-medium text-gray-700'>
+        <p>Booking slots</p>
+        <div className='flex gap-3 items-center w-full overflow-x-scroll mt-4'>
+          {
+            docSlots.length && docSlots.map((item,index)=>(
+              <div onClick={()=> setSlotIndex(index)} className={`text-center py-6 min-w-16 rounded-full cursor-pointer ${slotIndex === index ? 'bg-primary text-white' :'border border-gray-200'}`} key={index}>
+                <p>{item[0] && daysOfWeek[item[0].datetime.getDay()]}</p>
+                <p>{item[0] && item[0].datetime.getDate()}</p>
+              </div>
+            ))
+          }
+        </div>
+
+        {/* --------------TIme Slots---------------- */}
+        <div className='flex items-center gap-3 w-full overflow-x-scroll mt-4'>
+          {
+            docSlots.length && docSlots[slotIndex].map((item,index)=>(
+              <p onClick={()=> setSlotTime(item.time)} className={`text-sm font-light flex-shrink-0 px-5 py-2 rounded-full cursor-pointer ${item.time === slotTime ? 'bg-primary text-white' : 'text-gray-400 border border-gray-300'}`} key={index}>
+                {item.time.toLowerCase()}
+              </p>
+            ))
+          }
+        </div>
+      </div>
+
     </div>
   )
 }
