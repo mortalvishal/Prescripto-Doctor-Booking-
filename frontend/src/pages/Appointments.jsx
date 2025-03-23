24a9b4682506dbdc8602a66b0402080a8449ea1a
vishal pathak <vishalpathak842@gmail.com>
2025-03-23 13:33:25 +0530

    Add "Book an appointment" button to Appointments component

--- frontend/src/pages/Appointments.jsx
+++ frontend/src/pages/Appointments.jsx
@@ -122,6 +122,7 @@ const Appointments = () => {
             ))
           }
         </div>
+        <button className='bg-primary text-white text-sm font-light px-14 py-3 rounded-full my-6'>Book an appointment</button>
       </div>
 
     </div>
