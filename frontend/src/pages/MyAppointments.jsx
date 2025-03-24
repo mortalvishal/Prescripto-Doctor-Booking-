ed1ff19fc3eb87705c5a9a81244d78ad9a083636
vishal pathak <vishalpathak842@gmail.com>
2025-03-24 22:59:33 +0530

    Display a list of doctors with appointment details and action buttons in MyAppointments component

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -9,7 +9,27 @@ const MyAppointments = () => {
     <div>
        <p>My Appointments</p>
        <div>
-
+          {
+            doctors.slice(0,2).map((item,index)=>(
+              <div key={index}>
+                <div>
+                  <img src={item.image} alt=''/>
+                </div>
+                <div>
+                  <p>{item.name}</p>
+                  <p>{item.speciality}</p>
+                  <p>Address:</p>
+                  <p>{item.address.line1}</p>
+                  <p>{item.address.line2}</p>
+                  <p> <span>Date & Time:</span> 25, March, 2025 | 8:30 PM</p>
+                </div>
+                <div></div>
+                <div>
+                  <button>Pay Online</button>
+                  <button>Cancle appointment</button>
+                </div>
+              </div>
+            ))}
         </div> 
     </div>
   )
