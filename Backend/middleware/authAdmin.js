0ce1a2aacbe2ddff7881c89914ba7911ff23ae0b
vishal pathak <vishalpathak842@gmail.com>
2025-04-21 10:58:25 +0530

    Add admin authentication middleware for token verification

---
+++ Backend/middleware/authAdmin.js
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken";
+
+// admin authentication middleware
+const authAdmin = async (req, res, next) => {
+  try {
+    const { aToken } = req.headers;
+    if (!aToken) {
+      return res.json({
+        success: false,
+        message: "Not authorized login again",
+      });
+    }
+    const token_decode = jwt.verify(aToken, process.env.JWT_SECRET);
+    if (token_decode !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
+      return res.json({
+        success: false,
+        message: "Not authorized login again",
+      });
+    }
+
+    next();
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export default authAdmin;
