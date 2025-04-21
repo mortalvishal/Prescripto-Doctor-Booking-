b404068650dbca4b98c02ec52fbe63e69ae71545
vishal pathak <vishalpathak842@gmail.com>
2025-04-21 14:22:04 +0530

    Refactor App component for consistent formatting and improved readability

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,24 +1,24 @@
-import React, { useContext } from 'react'
-import Login from './pages/Login'
-import { ToastContainer, toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-import { AdminContext } from './context/AdminContext'
+import React, { useContext } from "react";
+import Login from "./pages/Login";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { AdminContext } from "./context/AdminContext";
+import Navbar from "./components/Navbar";
 
 function App() {
-
-  const {aToken} = useContext(AdminContext)
+  const { aToken } = useContext(AdminContext);
 
   return aToken ? (
-    <div >
-      <ToastContainer/>
-
+    <div className="bg-[#F8F9FD]">
+      <ToastContainer />
+      <Navbar />
     </div>
   ) : (
     <>
-    <Login/>
-    <ToastContainer/>
+      <Login />
+      <ToastContainer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
