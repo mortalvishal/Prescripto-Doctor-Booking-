8d6bb852834b2729ec110f60bbc2c6576245aeb6
vishal pathak <vishalpathak842@gmail.com>
2025-04-23 14:53:39 +0530

    Add AllAppointments, Dashboard, and DoctorsList components

---
+++ admin/src/pages/Admin/AllAppointments.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AllAppointments() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AllAppointments
---
+++ admin/src/pages/Admin/DoctorsList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DoctorsList() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DoctorsList
