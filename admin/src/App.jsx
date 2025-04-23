3848f81abec5969a99c87d3eee6e7b91681a7129
vishal pathak <vishalpathak842@gmail.com>
2025-04-23 09:44:17 +0530

    Refactor App component for improved readability and maintainability

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -4,6 +4,12 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { AdminContext } from "./context/AdminContext";
 import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import { Routes, Route } from 'react-router-dom'
+import Dashboard from "./pages/Admin/Dashboard";
+import AllAppointments from "./pages/Admin/AllAppointments";
+import DoctorsList from "./pages/Admin/DoctorsList";
+import AddDoctor from "./pages/Admin/AddDoctor";
 
 function App() {
   const { aToken } = useContext(AdminContext);
@@ -12,6 +18,16 @@ function App() {
     <div className="bg-[#F8F9FD]">
       <ToastContainer />
       <Navbar />
+      <div className="flex items-start">
+        <Sidebar/>
+        <Routes>
+          <Route path ='/' element={<></>} />
+          <Route path="/admin-dashboard" element = {<Dashboard/>} />
+          <Route path="/all-appointments" element={<AllAppointments/>} />
+          <Route path="/add-doctor" element={<AddDoctor/>} />
+          <Route path="/doctor-list" element={<DoctorsList/>} />
+        </Routes>
+      </div>
     </div>
   ) : (
     <>
