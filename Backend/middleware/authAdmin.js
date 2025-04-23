afa7e22ad7ae30bad08b79b888a16dfe436a493b
vishal pathak <vishalpathak842@gmail.com>
2025-04-23 14:29:55 +0530

    Fix variable name inconsistency in authAdmin middleware

--- Backend/middleware/authAdmin.js
+++ Backend/middleware/authAdmin.js
@@ -3,14 +3,14 @@ import jwt from "jsonwebtoken";
 // admin authentication middleware
 const authAdmin = async (req, res, next) => {
   try {
-    const { aToken } = req.headers;
-    if (!aToken) {
+    const { atoken } = req.headers;
+    if (!atoken) {
       return res.json({
         success: false,
         message: "Not authorized login again",
       });
     }
-    const token_decode = jwt.verify(aToken, process.env.JWT_SECRET);
+    const token_decode = jwt.verify(atoken, process.env.JWT_SECRET);
     if (token_decode !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
       return res.json({
         success: false,
