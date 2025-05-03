c773f98b358be946d0ee105ea1e34a0442f232e4
vishal pathak <vishalpathak842@gmail.com>
2025-05-03 22:28:50 +0530

    Add getDoctorsData function to AppContextProvider for fetching doctor data

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,16 +1,27 @@
 import { createContext } from "react";
 import { doctors } from "../assets/assets";
+import axios from "axios"
 
 export const AppContext = createContext()
 
 const AppContextProvider = (props) => {
 
     const currencySymbol = '₹';
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
 
     const value = {
         doctors,
         currencySymbol
     }
+
+    const getDoctorsData = async () => {
+        try {
+            const { data } = await 
+        } catch (error) {
+            
+        }
+    }
+
     return (
         <AppContext.Provider value={value}>
             {props.children}
