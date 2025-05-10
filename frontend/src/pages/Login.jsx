2e3295188ef2e3ad180abf98e45feb68a2154521
vishal pathak <vishalpathak842@gmail.com>
2025-05-10 18:13:36 +0530

    Implement login and registration functionality in Login component with token management

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,13 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { AppContext } from "../context/AppContext";
+import axios from 'axios'
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+
+  const { token, setToken, backendUrl } = useContext(AppContext)
+  const navigate = useNavigate()
   const [state, setState] = useState("Sign Up");
 
   const [email, setEmail] = useState("");
@@ -9,10 +16,40 @@ const Login = () => {
 
   const onSubmitHandler = async (event) => {
     event.preventDefault();
+
+    try {
+      
+      if (state === 'Sign Up') {
+        const { data } = await axios.post(backendUrl + '/api/user/register',{name, password,email})
+        if (data.success) {
+          localStorage.setItem('token', data.token)
+          setToken(data.token)
+        }else {
+          toast.error(data.message)
+        }
+      } else {
+        const { data } = await axios.post(backendUrl + '/api/user/login',{password,email})
+        if (data.success) {
+          localStorage.setItem('token', data.token)
+          setToken(data.token)
+        }else {
+          toast.error(data.message)
+        }
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
+
   };
 
+  useEffect(()=>{
+    if (token) {
+      navigate('/')
+    }
+  },[token])
+
   return (
-    <form className="min-h-[80vh] flex items-center">
+    <form onSubmit={onSubmitHandler} className="min-h-[80vh] flex items-center">
       <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-sm text-zinc-600 shadow-lg">
         <p className="text-2xl font-semibold">
           {state === "Sign Up" ? "Create Account" : "Login"}
@@ -27,7 +64,7 @@ const Login = () => {
             <input
               className="border border-zinc-300 rounded w-full p-2 mt-1"
               type="text"
-              onChange={(e) => setName(e.target.name)}
+              onChange={(e) => setName(e.target.value)}
               value={name}
             />
           </div>
@@ -38,7 +75,7 @@ const Login = () => {
           <input
             className="border border-zinc-300 rounded w-full p-2 mt-1"
             type="email"
-            onChange={(e) => setEmail(e.target.email)}
+            onChange={(e) => setEmail(e.target.value)}
             value={email}
           />
         </div>
@@ -47,11 +84,11 @@ const Login = () => {
           <input
             className="border border-zinc-300 rounded w-full p-2 mt-1"
             type="password"
-            onChange={(e) => setPassword(e.target.password)}
+            onChange={(e) => setPassword(e.target.value)}
             value={password}
           />
         </div>
-        <button className="bg-primary text-white w-full py-2 rounded-md text-base">
+        <button type="submit" className="bg-primary text-white w-full py-2 rounded-md text-base">
           {state === "Sign Up" ? "Create Account" : "Login"}
         </button>
         {state === "Sign Up" ? (
