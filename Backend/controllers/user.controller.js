21d9718e3dd8e39c86ee925a0e6147605e7aa5a4
vishal pathak <vishalpathak842@gmail.com>
2025-05-10 18:12:41 +0530

    Implement user registration API with validation and password hashing

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,9 +1,74 @@
-
-
+import validator from "validator";
+import bcrypt from "bcrypt";
+import userModel from "../models/user.model.js";
+import jwt from "jsonwebtoken";
 
 // API to register user from backend
 const registerUser = async (req, res) => {
   try {
-  } 
-  catch (error) {}
+    const { name, email, password } = req.body;
+
+    if (!name || !password || !email) {
+      return res.json({ success: false, message: " Missing Details " });
+    }
+    // validating email format
+    if (!validator.isEmail(email)) {
+      return res.json({ success: false, message: " Enter a valid email " });
+    }
+    // validating strong password
+    if (password.length < 8) {
+      return res.json({ success: false, message: " Enter a strong password " });
+    }
+
+    // Hashing user passwoord
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const userData = {
+      name,
+      email,
+      password: hashedPassword,
+    };
+
+    const newUser = new userModel(userData);
+    const user = await newUser.save();
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
 };
+
+// API for user login 
+const loginUser = async ( req, res ) => {
+  try {
+    
+    const { email, password } = req.body
+    const user = await userModel.findOne({email})
+
+    if (!user) {
+      return res.json({ success: false, message: ' User does not exits '});
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (isMatch) {
+      const token = jwt.sign({id:user._id}, process.env.JWT_SECRET)
+      res.json({ success: true, token})
+    }else {
+      res.json({ success: false, message:" Invalid Credentials "})
+    }
+
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+}
+
+// API to get user profile data 
+
+
+export {registerUser, loginUser}
