3bd72c15e2142b83b2e8cd0d0f5bbbbf56021cd9
vishal pathak <vishalpathak842@gmail.com>
2025-05-11 18:12:34 +0530

    Refactor loginUser function for improved readability and consistency

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -42,33 +42,30 @@ const registerUser = async (req, res) => {
   }
 };
 
-// API for user login 
-const loginUser = async ( req, res ) => {
+// API for user login
+const loginUser = async (req, res) => {
   try {
-    
-    const { email, password } = req.body
-    const user = await userModel.findOne({email})
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
 
     if (!user) {
-      return res.json({ success: false, message: ' User does not exits '});
+      return res.json({ success: false, message: " User does not exits " });
     }
 
-    const isMatch = await bcrypt.compare(password, user.password)
+    const isMatch = await bcrypt.compare(password, user.password);
 
     if (isMatch) {
-      const token = jwt.sign({id:user._id}, process.env.JWT_SECRET)
-      res.json({ success: true, token})
-    }else {
-      res.json({ success: false, message:" Invalid Credentials "})
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: " Invalid Credentials " });
     }
-
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
   }
-}
+};
 
-// API to get user profile data 
+// API to get user profile data
 
-
-export {registerUser, loginUser}
+export { registerUser, loginUser };
