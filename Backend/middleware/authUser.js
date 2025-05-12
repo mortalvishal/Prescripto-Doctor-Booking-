75383e8169c6b3490774ff5560c90a548bf07321
vishal pathak <vishalpathak842@gmail.com>
2025-05-12 23:55:19 +0530

    Fix authUser middleware token check logic to return error when token is missing

--- Backend/middleware/authUser.js
+++ Backend/middleware/authUser.js
@@ -4,7 +4,7 @@ import jwt from "jsonwebtoken";
 const authUser = async (req, res, next) => {
   try {
     const { token } = req.headers;
-    if (token) {
+    if (!token) {
       return res.json({
         success: false,
         message: "Not authorized login again",
