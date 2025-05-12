96177380f62f887d85c2b3836080041f74303549
vishal pathak <vishalpathak842@gmail.com>
2025-05-13 00:13:10 +0530

    Refactor user routes to improve code organization and add image upload support for updateProfile

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,17 +1,20 @@
-import express from 'express'
-import { registerUser, loginUser, getProfile } from '../controllers/user.controller.js'
-import authUser from '../middleware/authuser.js'
+import express from "express";
+import {
+  registerUser,
+  loginUser,
+  getProfile,
+  updateProfile,
+} from "../controllers/user.controller.js";
+import authUser from "../middleware/authuser.js";
+import upload from "../middleware/multer.js";
 //import authUser from '../middleware/authUser.js'
 
+const userRouter = express.Router();
 
+userRouter.post("/register", registerUser);
+userRouter.post("/login", loginUser);
 
-const userRouter = express.Router()
-
-userRouter.post('/register', registerUser)
-userRouter.post('/login',loginUser)
-userRouter.get('/get-profile',authUser ,getProfile)
-
-
-
+userRouter.get("/get-profile", authUser, getProfile);
+userRouter.post("/update-profile",upload.single("image"),authUser,updateProfile);
 
 export default userRouter;
