905263245a3de0c2e7e75d2493cda6eaf6d87bd1
vishal pathak <vishalpathak842@gmail.com>
2025-05-13 09:06:58 +0530

    Refactor MyProfile component to use context for user data and remove hardcoded values

--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,23 +1,12 @@
-import React, { useState } from "react";
-import { assets } from "../assets/assets";
+import React, { useContext, useState } from "react";
+import { AppContext } from "../context/AppContext";
 
 const MyProfile = () => {
-  const [userData, setUserData] = useState({
-    name: "Edward Vincent",
-    image: assets.profile_pic,
-    email: "richardjameswap@gmail.com",
-    phone: "+91 9876-5432-10",
-    address: {
-      line1: "57th Cross, Richmond",
-      line2: "Circle, Church Road, London",
-    },
-    gender: "Male",
-    dob: "06-08-2003",
-  });
-
+  
+  const {userData, setUserData} = useContext(AppContext)
   const [isEdit, setIsEdit] = useState(false);
 
-  return (
+  return userData && (
     <div className="max-w-lg flex flex-col gap-2 text-sm">
       <img className="w-36 rounded" src={userData.image} alt="" />
       {isEdit ? (
@@ -151,3 +140,5 @@ const MyProfile = () => {
 };
 
 export default MyProfile;
+
+
