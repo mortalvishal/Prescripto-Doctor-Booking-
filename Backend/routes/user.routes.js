65a26c86f0e5143b5972f075f3cf093cce2ba842
vishal pathak <vishalpathak842@gmail.com>
2025-05-15 13:38:53 +0530

    Add bookAppointment route to userRouter for appointment booking

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -4,6 +4,7 @@ import {
   loginUser,
   getProfile,
   updateProfile,
+  bookAppointment,
 } from "../controllers/user.controller.js";
 import authUser from "../middleware/authuser.js";
 import upload from "../middleware/multer.js";
@@ -16,5 +17,6 @@ userRouter.post("/login", loginUser);
 
 userRouter.get("/get-profile", authUser, getProfile);
 userRouter.post("/update-profile",upload.single("image"),authUser,updateProfile);
+userRouter.post("/book-appointment", authUser, bookAppointment)
 
 export default userRouter;
