05fbfb61829aec279d59e7626c0e161b0ed28077
vishal pathak <vishalpathak842@gmail.com>
2025-05-17 22:38:28 +0530

    Add cancel appointment route with user authentication

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -6,6 +6,7 @@ import {
   updateProfile,
   bookAppointment,
   listAppointment,
+  cancelAppointment,
 } from "../controllers/user.controller.js";
 import authUser from "../middleware/authuser.js";
 import upload from "../middleware/multer.js";
@@ -20,5 +21,6 @@ userRouter.get("/get-profile", authUser, getProfile);
 userRouter.post("/update-profile",upload.single("image"),authUser,updateProfile);
 userRouter.post("/book-appointment", authUser, bookAppointment)
 userRouter.get("/appointments",authUser,listAppointment)
+userRouter.post("/cancel-appointment", authUser, cancelAppointment)
 
 export default userRouter;
