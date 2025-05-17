ef44eadd2be4ed09d45fab22b0b4bc9809902486
vishal pathak <vishalpathak842@gmail.com>
2025-05-17 22:41:58 +0530

    Refactor MyAppointments component for improved readability and maintainability

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -94,6 +94,7 @@ const MyAppointments = () => {
               {!item.cancelled && <button onClick={()=>cancelAppointment(item._id)} className="text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-300">
                 Cancel appointment
               </button>}
+              {item.cancelled && <button className="sm:min-w-48 py-2 border border-red-500 rounded text-red-500">Appointment Cancelled</button>}
             </div>
           </div>
         ))}
