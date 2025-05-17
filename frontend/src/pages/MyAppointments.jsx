d4624c767362107da208f866c96611c034302dec
vishal pathak <vishalpathak842@gmail.com>
2025-05-17 22:38:36 +0530

    Implement cancel appointment functionality with user feedback in MyAppointments component

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -29,6 +29,23 @@ const MyAppointments = () => {
     }
   };
 
+  const cancelAppointment = async (appointmentId) => {
+    try {
+      
+      const { data } = await axios.post(backendUrl + "/api/user/cancel-appointment", {appointmentId},{headers:{token}})
+      if (data.success) {
+        toast.success(data.message)
+        getUserAppointments()
+      }else {
+        toast.error(data.message)
+      }
+
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message); 
+    }
+  }
+
   useEffect(() => {
     if (token) {
       getUserAppointments();
@@ -71,12 +88,12 @@ const MyAppointments = () => {
             </div>
             <div></div>
             <div className="flex flex-col gap-2 justify-end">
-              <button className="text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300">
+              {!item.cancelled && <button className="text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300">
                 Pay Online
-              </button>
-              <button className="text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-300">
-                Cancle appointment
-              </button>
+              </button>} 
+              {!item.cancelled && <button onClick={()=>cancelAppointment(item._id)} className="text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-300">
+                Cancel appointment
+              </button>}
             </div>
           </div>
         ))}
