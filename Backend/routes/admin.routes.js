728ea565a9cbc8f9d3e8adf4e8ed7aab79e1aa91
vishal pathak <vishalpathak842@gmail.com>
2025-05-22 21:24:08 +0530

    Add appointments route for admin to retrieve appointments

--- Backend/routes/admin.routes.js
+++ Backend/routes/admin.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addDoctor, allDoctors, loginAdmin } from "../controllers/admin.controller.js";
+import { addDoctor, allDoctors, appointmentsAdmin, loginAdmin } from "../controllers/admin.controller.js";
 import upload from "../middleware/multer.js";
 import authAdmin from "../middleware/authAdmin.js";
 import { changeAvailability } from "../controllers/doctor.controller.js";
@@ -10,5 +10,6 @@ adminRouter.post("/add-doctor", authAdmin, upload.single("image"), addDoctor);
 adminRouter.post("/login", loginAdmin);
 adminRouter.post('/all-doctors',authAdmin,allDoctors)
 adminRouter.post('/change-availability',authAdmin,changeAvailability)
+adminRouter.get("/appointments", authAdmin, appointmentsAdmin)
 
 export default adminRouter;
