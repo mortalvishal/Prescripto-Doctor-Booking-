13845e8d2cf5036f013ab5b1ed97abc18d405013
vishal pathak <vishalpathak842@gmail.com>
2025-05-23 22:30:15 +0530

    Add appointment cancellation API to manage appointment status and release doctor slots

--- Backend/controllers/admin.controller.js
+++ Backend/controllers/admin.controller.js
@@ -127,4 +127,36 @@ const appointmentsAdmin = async (req, res) => {
   }
 };
 
-export { addDoctor, loginAdmin, allDoctors, appointmentsAdmin };
+// API for appointment cancellation
+const appointmentCancel = async (req, res) => {
+  try {
+    const { appointmentId } = req.body;
+
+    const appointmentData = await appointmentModel.findById(appointmentId);
+
+    await appointmentModel.findByIdAndUpdate(appointmentId, {
+      cancelled: true,
+    });
+
+    // Releasing doctor slot
+    const { docId, slotDate, slotTime } = appointmentData;
+
+    const doctorData = await doctorModel.findById(docId);
+
+    let slots_booked = doctorData.slots_booked;
+
+    slots_booked[slotDate] = slots_booked[slotDate].filter(
+      (e) => e !== slotTime
+    );
+
+    await doctorModel.findByIdAndUpdate(docId, { slots_booked });
+
+    res.json({ success: true, message: "Appointment cancelled" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+
+export { addDoctor, loginAdmin, allDoctors, appointmentsAdmin, appointmentCancel };
