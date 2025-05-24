a83a17cb8d3ed24dd378e9721d5c77f3da751659
vishal pathak <vishalpathak842@gmail.com>
2025-05-24 08:25:20 +0530

    Add doctor login functionality to the Login component

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -3,6 +3,7 @@ import { assets } from "../assets/assets";
 import { AdminContext } from "../context/AdminContext";
 import axios from "axios";
 import { toast } from "react-toastify";
+import { DoctorContext } from "../context/DoctorContext";
 
 function Login() {
   const [state, setState] = useState("Admin");
@@ -10,6 +11,7 @@ function Login() {
   const [password,setPassword] = useState('')
 
   const {setAToken,backendUrl} = useContext(AdminContext)
+  const { setDToken } = useContext(DoctorContext)
 
   const onSubmitHandler = async (event) => {
     event.preventDefault();
@@ -24,7 +26,14 @@ function Login() {
                 toast.error(data.message)
             }
         } else {
-
+          const { data } = await axios.post(backendUrl + '/api/doctor/login',{email,password})
+          if (data.success) {
+            localStorage.setItem("dToken",data.token)
+            setDToken(data.token);
+            console.log(data.token);           
+        }else {
+            toast.error(data.message)
+        }
         }
     } catch (error) {
         
