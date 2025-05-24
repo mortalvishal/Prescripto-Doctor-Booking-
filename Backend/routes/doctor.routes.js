7b0151c2c6398b7990b6d50f7edd45716ed0465b
vishal pathak <vishalpathak842@gmail.com>
2025-05-24 14:09:10 +0530

    Add profile and update profile routes to doctorRouter for enhanced doctor management

--- Backend/routes/doctor.routes.js
+++ Backend/routes/doctor.routes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { appointmentComplete, appointmentsDoctor, doctorList, loginDoctor, appointmentCancel, doctorDashboard } from '../controllers/doctor.controller.js'
+import { appointmentComplete, appointmentsDoctor, doctorList, loginDoctor, appointmentCancel, doctorDashboard, doctorProfile, updateDoctorProfile } from '../controllers/doctor.controller.js'
 import authDoctor from '../middleware/authDoctor.js'
 
 const doctorRouter = express.Router()
@@ -10,5 +10,7 @@ doctorRouter.get('/appointments', authDoctor,appointmentsDoctor)
 doctorRouter.post('/complete-appointment', authDoctor, appointmentComplete)
 doctorRouter.post('/cancel-appointment', authDoctor, appointmentCancel)
 doctorRouter.get('/dashboard', authDoctor, doctorDashboard)
+doctorRouter.get('/profile', authDoctor, doctorProfile)
+doctorRouter.post('/update-profile', authDoctor, updateDoctorProfile)
 
 export default doctorRouter
