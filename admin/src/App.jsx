d3c5496691248de04b6c59f147db0346fc94218b
vishal pathak <vishalpathak842@gmail.com>
2025-05-24 10:01:02 +0530

    Add Doctor routes to App component for navigation

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -11,6 +11,9 @@ import AllAppointments from "./pages/Admin/AllAppointments";
 import DoctorsList from "./pages/Admin/DoctorsList";
 import AddDoctor from "./pages/Admin/AddDoctor";
 import { DoctorContext } from "./context/DoctorContext";
+import DoctorDashboard from "./pages/Doctor/DoctorDashboard";
+import DoctorAppointments from "./pages/Doctor/DoctorAppointments";
+import DoctorProfile from "./pages/Doctor/DoctorProfile";
 
 function App() {
   const { aToken } = useContext(AdminContext);
@@ -23,11 +26,18 @@ function App() {
       <div className="flex items-start">
         <Sidebar/>
         <Routes>
+
+          {/* Admin Routes */}
           <Route path ='/' element={<></>} />
           <Route path="/admin-dashboard" element = {<Dashboard/>} />
           <Route path="/all-appointments" element={<AllAppointments/>} />
           <Route path="/add-doctor" element={<AddDoctor/>} />
           <Route path="/doctor-list" element={<DoctorsList/>} />
+
+          {/* Doctor Routes */}
+          <Route path="/doctor-dashboard" element={<DoctorDashboard/>} />
+          <Route path="doctor-appointments" element={<DoctorAppointments/>}/>
+          <Route path="doctor-profile" element ={<DoctorProfile/>} />
         </Routes>
       </div>
     </div>
