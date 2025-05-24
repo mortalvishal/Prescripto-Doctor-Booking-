8ccffa667bd7cb59e4adccd720fd3c0cb637f3b3
vishal pathak <vishalpathak842@gmail.com>
2025-05-24 10:00:19 +0530

    Add appointments route with authentication to doctor router

--- Backend/routes/doctor.routes.js
+++ Backend/routes/doctor.routes.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { doctorList, loginDoctor } from '../controllers/doctor.controller.js'
+import { appointmentsDoctor, doctorList, loginDoctor } from '../controllers/doctor.controller.js'
+import authDoctor from '../middleware/authDoctor.js'
 
 const doctorRouter = express.Router()
 
 doctorRouter.get('/list', doctorList )
 doctorRouter.post('/login', loginDoctor)
+doctorRouter.get('/appointments', authDoctor,appointmentsDoctor)
 
 
 export default doctorRouter
